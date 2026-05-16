fc5c291b14819396ec91be07ad091f606ce7d574
seeun-jang <jangseeun108@gmail.com>
2026-05-17 02:42:35 +0900

    Add C programs for various arithmetic and logical operations

---
+++ Assignment05/Test1067.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void){
+	int a;
+	scanf("%d", &a);
+	
+	if (a > 0)
+	{
+		printf("plus\n");
+	}
+	else {
+		printf("minus\n");
+	}
+	if ( a % 2 == 0)
+	{
+		printf("even\n");
+	}
+	else {
+		printf("odd");
+	}
+	return 0;
+}
