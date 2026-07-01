1e2be5e46808a880bdf30dbea42529eaa0debd6b
seeun-jang <jangseeun108@gmail.com>
2026-07-01 23:06:42 +0900

    Add multiple Python programs for basic input/output and string manipulations

---
+++ PracticePython/1109.py
@@ -0,0 +1,16 @@
+print("mark")
+print(20)
+print("A")
+print(3.154)
+
+
+# 정확한 풀이의 답
+# name = input()
+# age = int(input())
+# code = input()
+# key = float(input())
+
+# print(name)
+# print(age)
+# print(code)
+# print(key)
