0419791f1d25ae9bc980151e4acff622311b8909
seeun-jang <jangseeun108@gmail.com>
2026-07-02 23:48:41 +0900

    Add multiple Python programs for basic arithmetic operations and calculations

---
+++ PracticePython/1116.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+print(f"{a}*{b}={a*b}")
+print(f"{a}/{b}={a//b}")
+
+# f-string
+# f가 붙으면 문자열 안에서 변수나 계산 결과를 넣을 수 있다.
